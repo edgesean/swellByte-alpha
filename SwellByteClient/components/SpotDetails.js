154c267944ce3f4b7f8dde8496152233cc8515aa
Sean Dillon <seandillon59@gmail.com>
2021-04-22 20:50:09 +0200

    Buoy Data fetch
    Added a fetch for the data buoy from my server. Started styling

--- SwellByteClient/components/SpotDetails.js
+++ SwellByteClient/components/SpotDetails.js
@@ -3,19 +3,25 @@ import { TouchableOpacity, StyleSheet, Text, View, SafeAreaView, ImageBackground
 import { EXPO_BUOY_DATA } from '@env';
 
 const SpotDetails = ({ route, navigation }) => {
-  const image = { uri: 'https://i.imgur.com/clZpR3S.png'}
+  const currentTime = new Date().getHours();
+  const image = { uri: 'https://i.imgur.com/clZpR3S.png'};
   const forecastData = route.params.data;
-  const [buoyData, setBuoy] = useState({});
+  const [buoyData, setBuoy] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const buoyFetch = async () => {
     let response = await fetch(EXPO_BUOY_DATA);
-    let json = await response.json();
-    // setBuoy(json);
-    console.log(json);
+    let jsonData = await response.json();
+    setBuoy(jsonData);
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 300)  
+    return
   }
   
   useEffect(() => {
-    buoyFetch();
+    buoyFetch()
+
   }, [])
 
   
@@ -23,21 +29,45 @@ const SpotDetails = ({ route, navigation }) => {
 
   return (
     <ImageBackground source={image} style={styles.image}>
-      <SafeAreaView>
+      {isLoading ?
+        <Text>Loading...</Text> :
+          <SafeAreaView style={styles.safeArea}>
 
-        <Text>Spot Details</Text>
 
-      </SafeAreaView>
+            <View style={styles.currentContainer}>
+
+            </View>
+
+          
+
+          </SafeAreaView>
+      }
     </ImageBackground>
   )
 }
 
 export default SpotDetails
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: 'cover',  
+  },
+  safeArea: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  currentContainer: {
+    flexDirection: 'column',
+    width: windowWidth-10,
+    height: 190,
+    borderWidth: 2,
+
+
   },
 
 })
