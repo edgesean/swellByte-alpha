c1733455de072bbb557d4daba5773d4a96f52028
Sean Dillon <seandillon59@gmail.com>
2021-04-22 17:30:15 +0200

    Styling

--- SwellByteClient/components/Home.js
+++ SwellByteClient/components/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import SpotPreview from './SpotPreview'
 import { EXPO_API_URL } from '@env';
@@ -26,41 +26,74 @@ const Home = () => {
   // isLoading && return <Loader></Loader>
   return (
     <ImageBackground source={image} style={styles.image}>
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView  style={styles.safe}>
       
           {isLoading ? 
         <Text>loading...</Text>  :
-        <SpotPreview forecastData={allData}/>
+        <View style={styles.container}>
+          <View style={styles.logo}>
+            <Text style={styles.logoText}>SwellByte</Text>
+          </View>
+          
+          <View style={styles.spots}>
+            <SpotPreview forecastData={allData}/>
+          </View>
+          
+        </View>
+        
         }
 
-      
-      
-        
-    
     </SafeAreaView>
     </ImageBackground>
   )
 }
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 
 const styles = StyleSheet.create({
   container: {
+    height: windowHeight,
+    width: windowWidth,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  safe: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-evenly',
-    height: '100%',
-    width: '100%',
-    
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: windowHeight,
+    width: windowWidth,
   },
   image: {
     flex:1,
     resizeMode: 'cover',
     justifyContent: 'center'
+  },
+  logo: {
+
+    margin: 5,
+    padding: 5,
+    width: '40%',
+    textAlign: 'center',
+  },
+  logoText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  spots: {
+    width: windowWidth-10,
+    height: windowHeight-60,
+    alignItems: 'center'
+
   }
 
 
-  
+
+
 });
 
 export default Home
--- SwellByteClient/components/SpotPreview.js
+++ SwellByteClient/components/SpotPreview.js
@@ -25,21 +25,27 @@ const SpotPreview = ({forecastData}) => {
   return (
     <View style={styles.container}>
 
-      <View style={styles.title}>
-        <Text >{spotName}</Text>
-      </View>
       
+        <Text style={styles.title} >{spotName}</Text>
+     
 
       <View style={styles.forecastFlex}>
+
         <View style={styles.waveContainer}>
-        <Text style={styles.text}>{`${height} m`}</Text>
-        <Text style={{transform: [{rotate: `${direction + 90}deg`}], color: 'white', fontSize: 20, fontWeight: 'bold'}} >➔</Text>
+          <Text style={styles.text}>{`${height} m`}</Text>
+          <Text style={{transform: [{rotate: `${direction + 90}deg`}], color: 'white', fontSize: 30, fontWeight: 'bold', width: 30}} >➔</Text>
+        </View>
+
+        <View style={styles.windContainer}>
+            <Text style={styles.text}>{`${windSpeed} km/h`} </Text>
+            <Text style={{transform: [{rotate: `${windDir + 90}deg`}], color: 'white', fontSize: 30, fontWeight: 'bold', width: 30}} >➔</Text>
+        </View>
+
       </View>     
-      <View style={styles.windContainer}>
-        <Text style={styles.text}>{`${windSpeed} km/h`} </Text>
-        <Text style={{transform: [{rotate: `${windDir + 90}deg`}], color: 'white', fontSize: 20, fontWeight: 'bold'}} >➔</Text>
-      </View>
-      </View>
+
+      
+
+      
       
       
       
@@ -58,34 +64,47 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 5,
     borderColor: 'white',
-    height: 100,
-    width: 150,
+    width: 310,
+    height: 150,
+    maxHeight: 150,
     margin: 10,
     padding: 1,
     marginTop: 15,
     backgroundColor: 'rgba(52, 52, 52, 0.01)',
   },
   text: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
     color: 'white',
   },
   title: {
-    flex: 1,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
 
   },
   forecastFlex: {
     alignItems: 'center',
+    justifyContent: 'center',
     flexDirection: 'row',
-    width: 300,
+    width: 347,
+    height: 70,
+    padding: 10,
+    margin: 5,
+
   },
 
   waveContainer: {
-    borderWidth: 2,
+    flex: 1,
+    alignItems: 'center',
+    width: '50%',
+   
 
   },
   windContainer: {
-    borderWidth: 2,
+    flex: 1,
+    alignItems: 'center',
+    width: '50%',
 
   },
 
